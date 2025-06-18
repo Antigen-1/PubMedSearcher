efab3adc11a2346e9d25a73fd90ccfbaa3d7cfae
ZhangHao <zhanghao@antigen.top>
2025-06-18 22:04:34 +0800

    Fix scheme codes

--- src/scm/dict.scm
+++ src/scm/dict.scm
@@ -5,8 +5,7 @@
             (#%scm-procedure 
                 (lambda (needed)
                     (let/cc exit
-                        (let ((nl (length needed))
-                              (result #hasheq{}))
+                        (let ((nl (length needed)))
                             (let loop ((i 0))
                                 (if (equal? i nl)
                                     'none
@@ -15,14 +14,15 @@
                                         (exit (make-exn (+ (@ needed i) ": not supported"))))))
                             (#%scm-procedure 
                                 (lambda (value)
-                                    (let loop ((i 0))
-                                        (if (equal? i nl)
-                                            result
-                                            (let ((field (@ needed i)))
-                                                (! result field 
-                                                (vm-apply (@ supported-string:accessor field)
-                                                             (tuple value)))
-                                                (loop (+ i 1))))))
+                                    (let ((result #hasheq{}))
+                                        (let loop ((i 0))
+                                            (if (equal? i nl)
+                                                result
+                                                (let ((field (@ needed i)))
+                                                    (! result field 
+                                                       (vm-apply (@ supported-string:accessor field)
+                                                                    (tuple value)))
+                                                    (loop (+ i 1)))))))
                                 1))))
         1)))
     2)
